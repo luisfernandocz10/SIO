cd9ff626b4eaaef8cad4a308dd05c804be580ebe
fer <luisfernandocz10@gmail.com>
2022-08-14 14:47:35 -0300

    eliminaos fer.cpp

--- fer.cpp
+++
@@ -1,7 +0,0 @@
-se reaiza una cambio en gt  casa 
-va mejira es viend com funciina un com mjt
-
-
-git config --global user.name "fer"
-git config --global user.email "luisfernandocz10@gmail.com"
-git config --list
